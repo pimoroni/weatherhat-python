2ff1e90a27b9b3c0d732b42c3429dbc9b39ce1e1
Phil Howard <phil@gadgetoid.com>
2022-01-22 14:29:36 +0000

    Add some tests

---
+++ library/tests/conftest.py
@@ -0,0 +1,58 @@
+import sys
+import pytest
+import mock
+
+
+@pytest.fixture(scope='function', autouse=True)
+def cleanup():
+    """This fixture removes modules under test from sys.modules.
+    This ensures that each module is fully re-imported, along with
+    the fixtures for each test function.
+    """
+
+    yield None
+    try:
+        del sys.modules["weatherhat"]
+    except KeyError:
+        pass
+
+
+@pytest.fixture(scope='function', autouse=False)
+def smbus2():
+    """Mock smbus2 module."""
+
+    smbus2 = mock.MagicMock()
+    smbus2.i2c_msg.read().__iter__.return_value = [0b00000000]
+    sys.modules['smbus2'] = smbus2
+    yield smbus2
+    del sys.modules['smbus2']
+
+
+@pytest.fixture(scope='function', autouse=False)
+def gpio():
+    sys.modules['RPi'] = mock.MagicMock()
+    sys.modules['RPi.GPIO'] = mock.MagicMock()
+    yield sys.modules['RPi.GPIO']
+    del sys.modules['RPi.GPIO']
+    del sys.modules['RPi']
+
+
+@pytest.fixture(scope='function', autouse=False)
+def bme280():
+    sys.modules['bme280'] = mock.MagicMock()
+    return sys.modules['bme280']
+    del sys.modules["bme280"]
+
+
+@pytest.fixture(scope='function', autouse=False)
+def ltr559():
+    sys.modules['ltr559'] = mock.MagicMock()
+    return sys.modules['ltr559']
+    del sys.modules["ltr559"]
+
+
+@pytest.fixture(scope='function')
+def ioe():
+    sys.modules['ioexpander'] = mock.MagicMock()
+    return sys.modules['ioexpander']
+    del sys.modules["ioexpander"]
---
+++ library/tests/test_setup.py
@@ -0,0 +1,39 @@
+def test_setup(gpio, ioe, bme280, ltr559, smbus2):
+    import weatherhat
+    library = weatherhat.WeatherHAT()
+
+    bus = smbus2.SMBus(1)
+
+    bme280.BME280.called_once_with(i2c_dev=bus)
+    ltr559.LTR559.called_once_with(i2c_dev=bus)
+    ioe.IOE.called_once_with(i2c_addr=0x12, interrupt_pin=4)
+
+    del library
+
+
+def test_api(gpio, ioe, bme280, ltr559, smbus2):
+    import weatherhat
+    library = weatherhat.WeatherHAT()
+
+    bus = smbus2.SMBus(1)
+
+    bme280.BME280(i2c_dev=bus).get_temperature.return_value = 20.0
+    bme280.BME280(i2c_dev=bus).get_pressure.return_value = 10600.0
+    bme280.BME280(i2c_dev=bus).get_humidity.return_value = 60.0
+
+    ltr559.LTR559(i2c_dev=bus).get_lux.return_value = 100.0
+
+    ioe.IOE(i2c_addr=0x12, interrupt_pin=4).input.return_value = 2.3
+
+    library.temperature_offset = 5.0
+
+    library.update()
+
+    assert library.wind_direction_raw == 2.3
+    assert library.wind_direction == 180
+    assert library.device_temperature == 20.0
+    assert library.temperature == 25.0
+    assert library.pressure == 10600.0
+    assert library.relative_humidity == 15.0
+    assert library.humidity == 60.0
+    assert library.lux == 100.0
